27638997204d733cd8173b8fd9726d5cb7fad211
Mike Nieva <m@mikenieva.com>
2024-01-20 11:38:08 -0600

    Prueba de shadcn en botón

--- app/mike/page.tsx
+++ app/mike/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function MikePage() {
-  return <div>Página de Mike</div>;
+  return (
+    <>
+      <div>Página de Mike</div>
+      <Button variant="mike" size="sm">
+        Este es mi botón que da dinero
+      </Button>
+    </>
+  );
 }
