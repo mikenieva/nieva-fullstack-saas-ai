87d06839c98f20f4458f11091dd9779963a7a9ba
Mike Nieva <m@mikenieva.com>
2024-01-20 11:26:42 -0600

    Generación de layout y práctica de ruta

---
+++ app/mike/layout.tsx
@@ -0,0 +1,3 @@
+export default function MikeLayout({ children }) {
+  return <div>{children}</div>;
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+// USO DE REACT
+
+export default function TestPage() {
+  return (
+    <>
+      <div>Este es un test</div>
+      <h1>Este es un encabezado</h1>
+    </>
+  );
+}
